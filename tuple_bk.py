ca0a160e1c445a000ec818b1fe901d2a664c5bd4
BabyX047 <hyperlaser93.lg@gmail.com>
2024-08-28 21:43:26 +0300

    Tuples version 1.0

---
+++ tuple_bk.py
@@ -0,0 +1,6 @@
+# Create a tuple of book names
+favorite_books = ("1984", "To Kill a Mockingbird", "Brave New World", "The Catcher in the Rye", "Pride and Prejudice")
+
+# Use a for loop to print each book name on a separate line
+for book in favorite_books:
+    print(book)
