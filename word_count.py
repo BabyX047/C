608b2e2952d9c0ba81e6147f0b634f139bd09fdc
BabyX047 <hyperlaser93.lg@gmail.com>
2024-08-31 22:54:12 +0300

    version 1.3

--- word_count.py
+++ word_count.py
@@ -4,5 +4,9 @@ words = input("Enter words separated by a space: ").split()
 # Use list comprehension to create a new list with words that have an odd number of characters
 odd_length_words = [word for word in words if len(word) % 2 != 0]
 
-# Print the new list
-print(odd_length_words)
+# Use list comprehension to create a new list with words that have an even number of characters
+even_length_words = [word for word in words if len(word) % 2 == 0]
+
+# Print the lists
+print("Odd words are: ", odd_length_words)
+print("Even words are: ", even_length_words)
