345c4ef6842c7a2a335e569378c70e31c92b4212
BabyX047 <hyperlaser93.lg@gmail.com>
2024-08-31 23:14:43 +0300

    Ver final

--- word_count.py
+++ word_count.py
@@ -3,7 +3,19 @@ words = []
 
 # Loop until the user has entered at least 5 words
 while len(words) < 5:
-    word = input("Enter a word: ")
+    # Determine the correct ordinal for the prompt
+    word_number = len(words) + 1
+    if word_number == 1:
+        prompt = "Enter the first word: "
+    elif word_number == 2:
+        prompt = "Enter the second word: "
+    elif word_number == 3:
+        prompt = "Enter the third word: "
+    else:
+        prompt = f"Enter the {word_number}th word: "
+
+    # Get the word from the user
+    word = input(prompt)
     words.append(word)  # Add the word to the list
 
 # Use list comprehension to create a new list with words that have an odd number of characters
@@ -13,5 +25,5 @@ odd_length_words = [word for word in words if len(word) % 2 != 0]
 even_length_words = [word for word in words if len(word) % 2 == 0]
 
 # Print the lists
-print("Odd words are: ", odd_length_words)
-print("Even words are: ", even_length_words)
+print("\nOdd words are: ", odd_length_words)
+print("\nEven words are: ", even_length_words)
