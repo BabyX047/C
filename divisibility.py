ad78b1c8c5760b74b04edfee03672e3627618861
BabyX047 <hyperlaser93.lg@gmail.com>
2024-09-02 18:31:27 +0300

    divisibility ver 1.0

---
+++ divisibility.py
@@ -0,0 +1,21 @@
+def divisible_by_ten(num):
+    # Calculate the remainder when num is divided by 10
+    remainder = num % 10
+    
+    # Check if the remainder is 0
+    if remainder == 0:
+        return True
+    else:
+        return False
+
+
+# Get user input
+
+testnum = int(input())
+
+# Check if the number is divisible by 10
+
+if divisible_by_ten(testnum):
+    print(str(testnum) + " is divisible by 10")
+else:
+    print(str(testnum) + " is not divisible by 10")
