c607331df2693bf8e21c4cf4be73299dafeae7a2
BabyX047 <hyperlaser93.lg@gmail.com>
2024-09-04 23:24:10 +0300

    age

--- Hello_world.py
+++ Hello_world.py
@@ -3,4 +3,8 @@ print("\nHello, There!")
 
 name = input("\nWhat is your name: " + str())
 
-print("\nWelcome, " + name + "!\n\n")
+print("\nWelcome, " + name + "!\n")
+
+age = input("How old are you: " + str())
+
+print("\nYour age is " + age + ".\n\n")
