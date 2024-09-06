cba350019d280bcced79202bef809de5ff79e5bc
BabyX047 <hyperlaser93.lg@gmail.com>
2024-09-06 22:54:26 +0300

    ver 1.1

--- conditional.py
+++ conditional.py
@@ -10,5 +10,14 @@ age = int(input("Enter your age: "))
 # Check if the age is greater than or equal to 18
 if age >= 18:
     print("You are eligible to vote")
+
+    choicetovote = input("Do you want to vote? Y/N \n").lower()
+
+    if choicetovote == 'y':
+        print("Great, Let's exercise democracy! ")
+    else:
+        print("We'll try next time, but dont complain.")
+
+
 else:
     print("You are not eligible to vote")
