15093c28a1106c6c001bbbc7765f1fabdc795318
BabyX047 <hyperlaser93.lg@gmail.com>
2024-09-15 12:50:56 +0300

    Going okay so far

--- Hello.c
+++ Hello.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
 int main() {
-    printf("Hello World");
+
+    char name[50];  
+    
+    printf("Hello World \n");
+
+    printf("My name is Computer. What is Yours? \n");
+    scanf("%s", name);
+
+    printf("\nWelcome to C, %s! Have fun coding.\n", name);
 
     return 0;
 }
