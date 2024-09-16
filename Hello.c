b7cb285075ef965004f4d8b6d68e92334c8aed23
BabyX047 <hyperlaser93.lg@gmail.com>
2024-09-16 20:43:02 +0300

    I learnt strcmp

--- Hello.c
+++ Hello.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
+#include <string.h>  // Needed for strcmp function
 
 int main() {
 
     char name[50];  
     int age;
+    char yorn[3];  // To store "Y" or "N"
     
     printf("Hello World \n");
 
@@ -19,6 +21,19 @@ int main() {
         printf("Please enter a valid age. You should be at least 6 years old.\n");
     } else if (age >= 6 && age <= 14) {
         printf("You could learn Scratch.\n");
+        printf("Do you want to begin right away? Y/N: ");
+        scanf("%s", yorn);
+
+        // String comparison using strcmp
+        if (strcmp(yorn, "Y") == 0 || strcmp(yorn, "y") == 0) {
+            printf("Great, we'll get right to it!\n");
+        } else if (strcmp(yorn, "N") == 0 || strcmp(yorn, "n") == 0) {
+            printf("No worries, we can begin when you are ready.\n");
+        } else {
+            printf("Please enter a valid option.\n");
+        }
+
+        
     } else if (age >= 15 && age <= 30) {
         printf("You could learn any other programming language.\n");
     } else {
