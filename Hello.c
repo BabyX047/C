83f40ec92b6faf5ddb10d5e3efc03655bdc36fc7
BabyX047 <hyperlaser93.lg@gmail.com>
2024-09-16 20:34:15 +0300

    C is fun

--- Hello.c
+++ Hello.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+
 int main() {
 
     char name[50];  
@@ -11,13 +12,17 @@ int main() {
 
     printf("\nWelcome to C, %s! Have fun coding.\n", name);
 
-    printf("How old are you, %s?", name);
-    scanf("%d", age);
+    printf("How old are you, %s? ", name);
+    scanf("%d", &age);
 
-    if (0 > age > 14) {
-        printf("You could learn Scratch");
-    }else {
-        printf("you can learn any other language");
+    if (age < 6) {
+        printf("Please enter a valid age. You should be at least 6 years old.\n");
+    } else if (age >= 6 && age <= 14) {
+        printf("You could learn Scratch.\n");
+    } else if (age >= 15 && age <= 30) {
+        printf("You could learn any other programming language.\n");
+    } else {
+        printf("Please enter a valid age.\n");
     }
 
     return 0;
